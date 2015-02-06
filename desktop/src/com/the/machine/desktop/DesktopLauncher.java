6267ac0048a9e6015b2c29bd92e87a5762559e78
Fabian Fränz <f.fraenz@t-online.de>
2015-02-07 00:36:01 +0100

    Initial setup.

---
+++ desktop/src/com/the/machine/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.the.machine.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.the.machine.TheMachine;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TheMachine(), config);
+	}
+}
