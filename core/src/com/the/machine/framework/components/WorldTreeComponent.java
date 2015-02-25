fb0ae2a96e7b24fc05c50ce1d03851fd6fe26f91
FransM22 <FransM22@users.noreply.github.com>
2015-02-25 13:46:33 +0100

    Github test.

--- core/src/com/the/machine/framework/components/WorldTreeComponent.java
+++ core/src/com/the/machine/framework/components/WorldTreeComponent.java
@@ -3,7 +3,7 @@ package com.the.machine.framework.components;
 import com.badlogic.ashley.core.Component;
 
 /**
- * TODO Add description
+ * An entity has the WorldTreeComponent if it is shown in the WorldTree in the EditorScene
  *
  * @author Fabian Fraenz <f.fraenz@t-online.de>
  * @created 21/02/15
