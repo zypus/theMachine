57937b5ca57b619489f3b43b37a5f1b2194d66d0
Fabian Fränz <f.fraenz@t-online.de>
2015-05-04 13:18:18 +0200

    Added a small debug window, to see the current coverage. Currently the bot only likes to go to the center. Evaluation strategy needs to be reworked.

--- core/src/com/the/machine/behaviours/MapCoverBehaviour.java
+++ core/src/com/the/machine/behaviours/MapCoverBehaviour.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.the.machine.components.AreaComponent;
 import com.the.machine.components.BehaviourComponent;
 import com.the.machine.components.DiscreteMapComponent;
+import com.the.machine.debug.MapDebugWindow;
 import com.the.machine.framework.utility.Utils;
 import com.the.machine.misc.Placebo;
 import lombok.Data;
@@ -29,6 +30,7 @@ public class MapCoverBehaviour implements BehaviourComponent.Behaviour<MapCoverB
 		// if the state is null, that means that this is the first time the behaviour is evaluate, so lets construct the first state
 		if (state == null) {
 			state = generateState(context);
+			MapDebugWindow.debug(state.coverage, 0.5f);
 		}
 		updateCoverage(context, state);
 		Vector2 centerOfGravitation = determineGlobalCenterOfGravitation(state);
@@ -52,7 +54,7 @@ public class MapCoverBehaviour implements BehaviourComponent.Behaviour<MapCoverB
 			for (int j = 0; j < coverage[0].length; j++) {
 				// ignores values which are 1, (values which are 2 and should be ignored anyway)
 				if (coverage[i][j] < 1) {
-					coverage[i][j] += 0.0001f;
+					coverage[i][j] += 0.001f;
 					// makes sure the number is never bigger than 1
 					if (coverage[i][j] > 1) {
 						coverage[i][j] = 1;
@@ -70,15 +72,21 @@ public class MapCoverBehaviour implements BehaviourComponent.Behaviour<MapCoverB
 		// check the square created by the vision range around the player
 		for (int i = 0; i < range; i++) {
 			for (int j = 0; j < range; j++) {
-				int dx = i + range/2;
-				int dy = j + range/2;
+				int dx = i - range/2;
+				int dy = j - range/2;
 				// check if field is in vision range
 				if (dx*dx+dy*dy < range2) {
 					Vector2 lookDir = new Vector2(dx, dy).nor();
 					float angle = Math.abs(lookDir.angle(direction));
 					// check if the vision angle covers that direction
 					if (angle < context.getVisionAngle()/2) {
-						coverage[(int) pos.x + dx][(int)pos.y + dy] = 0;
+							int xx = (int) pos.x + dx;
+							int yy = (int) pos.y + dy;
+						if (Utils.isInbound(xx, yy, 0, 0, coverage.length-1, coverage[0].length-1)) {
+							if (coverage[xx][yy] <= 1) {
+								coverage[xx][(yy)] = 0;
+							}
+						}
 					}
 				}
 			}
@@ -143,8 +151,8 @@ public class MapCoverBehaviour implements BehaviourComponent.Behaviour<MapCoverB
 		for (DiscreteMapComponent.MapCell cell : map) {
 			Vector2 position = cell.getPosition().cpy().add(offset);
 			AreaComponent.AreaType type = cell.getType();
-			if (type != OUTER_WALL && type != WALL) {
-				boolean inbound = Utils.isInbound(position.x, position.y, 0, 0, width, height);
+			if (type == OUTER_WALL || type == WALL) {
+				boolean inbound = Utils.isInbound(position.x, position.y, 0, 0, width-1, height-1);
 				if (inbound) {
 					area[(int)position.x][(int)position.y] = WALL;
 				}
@@ -152,7 +160,12 @@ public class MapCoverBehaviour implements BehaviourComponent.Behaviour<MapCoverB
 		}
 		// perform a flood fill from the current position to determine all reachable areas
 		Vector2 myPos = placebo.getPos();
-		floodFill(area, (int)(myPos.x+offset.x), (int)(myPos.y+offset.y));
+		List<Vector2> next = new ArrayList<>();
+		next.add(myPos.cpy()
+					  .add(offset));
+		while (!next.isEmpty()) {
+			floodFill(area, next);
+		}
 		// construct a coverage map from the obtained map analysis
 		float[][] coverage = new float[width][height];
 		for (int i = 0; i < width; i++) {
@@ -173,16 +186,22 @@ public class MapCoverBehaviour implements BehaviourComponent.Behaviour<MapCoverB
 	// orthogonal direction for the flood fill
 	private static Vector2[] dirs = new Vector2[]{new Vector2(1, 0), new Vector2(0 , 1), new Vector2(-1, 0), new Vector2(0,-1) };
 
-	private void floodFill(AreaComponent.AreaType[][] area, int x, int y) {
-		// if this spot is a target area, mark it as visited
-		if (area[x][y] == TARGET) {
-			area[x][y] = GROUND;
-			// flood continues on the adjacent fields
-			for (Vector2 dir : dirs) {
-				int nx = (int) (x + dir.x);
-				int ny = (int) (y + dir.y);
-				if (Utils.isInbound(nx, ny, 0, 0, area.length, area[0].length )) {
-					floodFill(area, x, y);
+	private void floodFill(AreaComponent.AreaType[][] area, List<Vector2> next) {
+		int x,y;
+		if (!next.isEmpty()) {
+			Vector2 vector2 = next.remove(0);
+			x = (int)vector2.x;
+			y = (int)vector2.y;
+			// if this spot is a target area, mark it as visited
+			if (area[x][y] == TARGET) {
+				area[x][y] = GROUND;
+				// flood continues on the adjacent fields
+				for (Vector2 dir : dirs) {
+					int nx = (int) (x + dir.x);
+					int ny = (int) (y + dir.y);
+					if (Utils.isInbound(nx, ny, 0, 0, area.length - 1, area[0].length - 1)) {
+						next.add(new Vector2(nx, ny));
+					}
 				}
 			}
 		}
---
+++ core/src/com/the/machine/debug/MapDebugWindow.java
@@ -0,0 +1,82 @@
+package com.the.machine.debug;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+
+/**
+ * TODO Add description
+ *
+ * @author Fabian Fraenz <f.fraenz@t-online.de>
+ * @created 04/05/15
+ */
+public class MapDebugWindow
+		extends JFrame {
+
+	private static final float SCALE = 2;
+	private static Thread active;
+
+	private MapDebugWindow(float[][] map) {
+		this.setBounds(0, 0, (int)(map.length*SCALE), (int)(map[0].length*SCALE));
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		this.setTitle("Map Debugger");
+		this.setContentPane(new DebugPanel(map, SCALE));
+		this.setVisible(true);
+	}
+
+	public static void debug(float[][] map, float interval) {
+		MapDebugWindow window = new MapDebugWindow(map);
+		Thread updateLoop = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						Thread.sleep((long) (interval * 1000));
+						window.repaint();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		updateLoop.start();
+		active = updateLoop;
+	}
+
+	private static class DebugPanel extends JPanel {
+
+		private float[][] map;
+		private int width;
+		private int height;
+		private float scale;
+
+		public DebugPanel(float[][] map, float scale) {
+			this.map = map;
+			width = map.length;
+			height = map[0].length;
+			this.scale = scale;
+		}
+
+		@Override
+		public void paint(Graphics g) {
+			super.paint(g);
+			Graphics2D g2 = (Graphics2D) g;
+			Rectangle2D pixel = new Rectangle2D.Double(0,0,1,1);
+			for (int i = 0; i < width; i++) {
+				for (int j = 0; j < height; j++) {
+					float v = map[i][height-1-j];
+					if (v > 1) {
+						g2.setColor(new Color(0,0,1));
+					} else {
+						g2.setColor(new Color(v, 0, 0));
+					}
+					pixel.setRect(i*scale, j*scale, 1*scale, 1*scale);
+					g2.fill(pixel);
+				}
+			}
+		}
+	}
+}
