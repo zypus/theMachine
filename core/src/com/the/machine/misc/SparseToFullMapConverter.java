ce5faa8d2aef311764739b27e55741521ec37e41
Fabian Fränz <f.fraenz@t-online.de>
2015-05-13 14:51:44 +0200

    Added a static class to convert from the sparse map representation to the fully expressed 2d area type array map representation. Is it also possible to update a allready created map, to keep track of changes like open/closed doors and windows etc.

---
+++ core/src/com/the/machine/misc/SparseToFullMapConverter.java
@@ -0,0 +1,10 @@
+package com.the.machine.misc;
+
+/**
+ * TODO Add description
+ *
+ * @author Fabian Fraenz <f.fraenz@t-online.de>
+ * @created 13/05/15
+ */
+public class SparseToFullMapConverter {
+}
