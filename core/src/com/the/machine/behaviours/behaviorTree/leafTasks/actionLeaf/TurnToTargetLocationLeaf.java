45dab22395039312427bbddb9d1c1d77f40c1892
Scattered-Rain <Florian221@gmx.net>
2015-05-18 12:46:13 +0200

    Fixed the turning bug

--- core/src/com/the/machine/behaviours/behaviorTree/leafTasks/actionLeaf/TurnToTargetLocationLeaf.java
+++ core/src/com/the/machine/behaviours/behaviorTree/leafTasks/actionLeaf/TurnToTargetLocationLeaf.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 
 import com.badlogic.gdx.ai.btree.LeafTask;
 import com.badlogic.gdx.ai.btree.Task;
+import com.badlogic.gdx.math.Vector2;
 import com.the.machine.behaviours.behaviorTree.TreeContext;
 import com.the.machine.systems.ActionSystem;
 
@@ -15,7 +16,8 @@ public class TurnToTargetLocationLeaf extends LeafTask<TreeContext>{
 	@Override
 	public void run(TreeContext context) {
 		if(object.getTargetLocation()!=null){
-			context.addResponse(ActionSystem.Action.TURN, new ActionSystem.TurnData(context.getBehaviorContext().getMoveDirection().cpy().rotate(context.getBehaviorContext().getMoveDirection().angle(context.getTargetLocation())-90), speed));
+			Vector2 dir = context.getBehaviorContext().getMoveDirection().cpy().rotate(context.getBehaviorContext().getMoveDirection().angle(context.getTargetLocation()) + context.getBehaviorContext().getMoveDirection().angle());
+			context.addResponse(ActionSystem.Action.TURN, new ActionSystem.TurnData(dir, speed));
 			context.setTargetLocation(null);
 			super.success();
 		}
