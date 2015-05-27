099ab0380a769643a665d14bf8d9ef8d16f88b5f
Scattered-Rain <Florian221@gmx.net>
2015-05-27 15:06:05 +0200

    sdg

--- core/src/com/the/machine/behaviours/behaviorTree/leafTasks/actionLeaf/MapCoverageLeaf.java
+++ core/src/com/the/machine/behaviours/behaviorTree/leafTasks/actionLeaf/MapCoverageLeaf.java
@@ -2,13 +2,23 @@ package com.the.machine.behaviours.behaviorTree.leafTasks.actionLeaf;
 
 import com.badlogic.gdx.ai.btree.LeafTask;
 import com.badlogic.gdx.ai.btree.Task;
+import com.badlogic.gdx.math.Vector2;
 import com.the.machine.behaviours.behaviorTree.TreeContext;
+import com.the.machine.map.Mapper;
 
 public class MapCoverageLeaf extends LeafTask<TreeContext>{
+	
+	private Mapper mapper;
 
 	@Override
 	public void run(TreeContext context) {
-		//TODO: Add Interaction With Map Coverage Here!
+		if(this.mapper==null){
+			initMapper(context);
+		}
+	}
+	
+	private void initMapper(TreeContext context){
+		
 	}
 	
 	@Override
