1823dc9d474a0b90b661ac0366e81719b4b8c4a6
Scattered-Rain <Florian221@gmx.net>
2015-06-15 15:31:09 +0200

    -

--- core/src/com/the/machine/behaviours/behaviorTree/leafTasks/actionLeaf/DoubleHearing.java
+++ core/src/com/the/machine/behaviours/behaviorTree/leafTasks/actionLeaf/DoubleHearing.java
@@ -26,9 +26,10 @@ public class DoubleHearing extends LeafTask<TreeContext>{
 	
 	@Override
 	public void run(TreeContext context) {
-		addNewSound(context);
 		timeStep++;
+		addNewSound(context);
 		deleteSounds();
+		checkDouble(context);
 	}
 	
 	private void addNewSound(TreeContext context){
@@ -41,13 +42,19 @@ public class DoubleHearing extends LeafTask<TreeContext>{
 					alreadyIn = true;
 				}
 			}
+			
 			if(!alreadyIn){
 				add(newSound);
 			}
 		}
 	}
 	
-	
+	private void checkDouble(TreeContext context){
+		List<Vector2> potSecSounds = context.getBehaviorContext().getSoundDirections();
+		for(Vector2 secSound : potSecSounds){
+			
+		}
+	}
 	
 	private void deleteSounds(){
 		for(int c=0; c<recentSounds.size(); c++){
