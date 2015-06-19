46bd3fda87a85c2226bb55aa8fae0ea4e8ac4ee4
Fabian Fränz <f.fraenz@t-online.de>
2015-06-19 13:45:49 +0200

    changed agent types.

--- core/src/com/the/machine/systems/MapSystem.java
+++ core/src/com/the/machine/systems/MapSystem.java
@@ -16,7 +16,7 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Filter;
 import com.badlogic.gdx.utils.TimeUtils;
-import com.the.machine.behaviours.MapCoverBehaviour;
+import com.the.machine.behaviours.behaviorTree.TreeBehavior;
 import com.the.machine.components.AgentComponent;
 import com.the.machine.components.AgentSightComponent;
 import com.the.machine.components.AngularVelocityComponent;
@@ -475,8 +475,8 @@ public class MapSystem
 				newAgent.add(new NameComponent().setName("Agent"));
 
 
-//				newAgent.add(new BehaviourComponent<TreeBehavior.TreeBehaviorState>().setBehaviour(new TreeBehavior()));
-				newAgent.add(new BehaviourComponent<MapCoverBehaviour.MapCoverBehaviourState>().setBehaviour(new MapCoverBehaviour()));
+				newAgent.add(new BehaviourComponent<TreeBehavior.TreeBehaviorState>().setBehaviour(new TreeBehavior()));
+//				newAgent.add(new BehaviourComponent<MapCoverBehaviour.MapCoverBehaviourState>().setBehaviour(new MapCoverBehaviour()));
 				//TODO: Add Working Behaviour
 
 
